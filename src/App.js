93aefc6bbc4515abbbc33b6c3ea293979135f135
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-23 17:38:09 -0400

    My attempt to filter monsters list based on search text.

--- src/App.js
+++ src/App.js
@@ -9,14 +9,25 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      monstersFiltered: [],
+      // filter: 'Leanne',
     }
+    console.clear()
+    console.log('1 constructor')
   }
 
   componentDidMount() {
+    console.log('3 componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
+
     .then((users) => this.setState(() => {
-      return {monsters: users}
+
+      // users.filter(el => el.name.includes(this.state.filter))
+      // console.log(users[5].name)
+      console.log(users)
+
+      return {monsters: users, monstersFiltered: users}
     },
     () => {
       console.log(this.state)
@@ -25,15 +36,32 @@ class App extends Component {
   }
 
   render() {
+    console.log('2 render')
+    console.log(this.state)
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
+        <input className='search-box' type='search' placeholder='Search monsters' onChange={(event) => {
+
+
+          const filteredMonsters = this.state.monsters.filter( (mon) => mon.name.includes(event.target.value))
+
+          // console.log(event.target.value)
+          this.setState(() => {
+            return {
+              // filter: event.target.value
+              monstersFiltered: filteredMonsters
+            }
           })
-        }
+        }} />
+
+        {
+        this.state.monstersFiltered.map((monster) => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        })
+      }
+
       </div>
     );
   }
