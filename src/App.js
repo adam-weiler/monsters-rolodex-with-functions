0ba03d93dc9a2e119f52ddfb575f9f1d20ac57d3
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-24 13:43:17 -0400

    Creating new directory for components. Importing new card-list.jsx component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -71,6 +71,7 @@ class App extends Component {
           })
         }
 
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+    render() {
+        return (
+            <div>
+                This is the CardList component!
+            </div>
+        )
+    }
+}
+
+export default CardList;
