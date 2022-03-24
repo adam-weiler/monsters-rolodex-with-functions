7f9370b46a6b2fb8b50af9062f3c14ff70c43992
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-24 16:58:38 -0400

    Using CSS to decorate card-list-component.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import './card-list.styles.css';
+
 //This component renders our monster list from the filteredMonsters list.
 class CardList extends Component {
     render() {
@@ -7,9 +9,15 @@ class CardList extends Component {
         
         return (
             <div className='card-list'>
-                {monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                {monsters.map(monster => {
+                    const { name, email, id } = monster; //Destructure the monster to variables to make code cleaner.
+                    return (
+                        <div className='card-container' key={id}>
+                            <img alt={`monster' ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )})}
             </div>
         )
     }
