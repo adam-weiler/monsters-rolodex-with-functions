ea7a8cee4fe31fecb44f83b42947d48edb8baf79
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-24 13:27:36 -0400

    Moving the function so it only loads once with the component, not on every page redraw.

--- src/App.js
+++ src/App.js
@@ -33,36 +33,43 @@ class App extends Component {
     ));
   }
 
+  // This function will only load once.
+  // This is better for performance than if it was attached to onChange() and needed to reload everytime the page rerenders.
+  onSearchChange = (event) => { // Whenever the user types in the text box, the monsters displayed are filtered down.
+
+    // console.log(event.target.value);
+
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return {
+        //searchField: searchField
+        searchField
+      }
+    })
+  }
+
   render() {
     console.log('2 render')
     // console.log(this.state)
 
-    const filteredMonsters = this.state.monsters.filter( (mon) => mon.name.toLocaleLowerCase().includes(this.state.searchField));
+    //Destructuring, so we don't need to call 'this' everywhere. Makes things more readable too.
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter( (mon) => mon.name.toLocaleLowerCase().includes(searchField));
 
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='Search monsters' onChange={(event) => {
-
-          // console.log(event.target.value);
-
-          const searchField = event.target.value.toLowerCase();
-
-          this.setState(() => {
-            return {
-              //searchField: searchField
-              searchField
-            }
-          })
-        }} />
+        <input className='search-box' type='search' placeholder='Search monsters' onChange={onSearchChange} />
 
         {
-        filteredMonsters.map((monster) => {
-          return <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        })
-      }
+          filteredMonsters.map((monster) => {
+            return <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          })
+        }
 
       </div>
     );
