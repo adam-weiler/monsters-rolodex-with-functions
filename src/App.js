1d7803980f2db2db62538f1d81704f07b4d03b86
Adam Weiler <adam.weiler@yahoo.ca>
2022-03-25 12:44:40 -0400

    Starting the process to convert app from class components into functional components.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,83 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchField: '',
-    }
-    console.clear()
-    // console.log('1 constructor')
-  }
-
-  componentDidMount() {
-    // console.log('3 componentDidMount')
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-
-    .then((users) => this.setState(() => {
-      // console.log(users)
-      return {monsters: users}
-    }
-    ));
-  }
+const App = () => {
+  console.log('render')
+  const [searchField, setSearchField] = useState('');  // Gives us back an array of 2 values. [value, setValue()]
+  console.log( searchField );
 
   // This function will only load once.
   // This is better for performance than if it was attached to onChange() and needed to reload everytime the page rerenders.
-  onSearchChange = (event) => { // Whenever the user types in the text box, the monsters displayed are filtered down.
-
-    // console.log(event.target.value);
-
-    const searchField = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      return {
-        //searchField: searchField
-        searchField
-      }
-    })
+  const onSearchChange = (event) => { // Whenever the user types in the text box, the monsters displayed are filtered down.
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   }
 
-  render() {
-    // console.log('2 render')
-    // console.log(this.state)
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
 
-    //Destructuring, so we don't need to call 'this' everywhere. Makes things more readable too.
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter( (mon) => mon.name.toLocaleLowerCase().includes(searchField));
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-
-        <SearchBox 
-          className='monsters-search-box'
-          onChangeHandler={onSearchChange} 
-          placeholder='search monsters'
-        />
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
-  }
+      <SearchBox 
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange} 
+        placeholder='search monsters'
+      />
+      
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     }
+//     console.clear()
+//     // console.log('1 constructor')
+//   }
+
+//   componentDidMount() {
+//     // console.log('3 componentDidMount')
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+
+//     .then((users) => this.setState(() => {
+//       // console.log(users)
+//       return {monsters: users}
+//     }
+//     ));
+//   }
+
+  
+
+//   render() {
+//     // console.log('2 render')
+//     // console.log(this.state)
+
+//     //Destructuring, so we don't need to call 'this' everywhere. Makes things more readable too.
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters.filter( (mon) => mon.name.toLocaleLowerCase().includes(searchField));
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+
+//         <SearchBox 
+//           className='monsters-search-box'
+//           onChangeHandler={onSearchChange} 
+//           placeholder='search monsters'
+//         />
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
